ab08d6d86c2b61cfc1bf94dfa4ed3158f0ef5ce2
AniaTofik <anna.kiedel@onet.pl>
2022-04-02 12:04:54 +0200

    created task.dto

--- projects/tasks/src/lib/adapters/primary/ui/form-task.component.ts
+++ projects/tasks/src/lib/adapters/primary/ui/form-task.component.ts
@@ -1,7 +1,16 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ViewEncapsulation,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 
-@Component({ selector: 'lib-form-task', templateUrl: './form-task.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+@Component({
+  selector: 'lib-form-task',
+  templateUrl: './form-task.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
 export class FormTaskComponent {
-  readonly taskForm: FormGroup = new FormGroup({text: new FormControl()});
+  readonly taskForm: FormGroup = new FormGroup({ text: new FormControl() });
 }
---
+++ projects/tasks/src/lib/application/ports/secondary/task.dto.ts
@@ -0,0 +1,3 @@
+export interface TaskDTO {
+  readonly text: string;
+}
