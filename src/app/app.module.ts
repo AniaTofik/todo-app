24037a645d95ca684fad01ca4af011d12b204d71
AniaTofik <anna.bodal@op.pl>
2022-05-17 09:50:15 +0200

    changeed file enviroments and app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponentModule } from '@navigation';
 import { DateComponentModule } from '@date';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 
 @NgModule({
@@ -16,7 +17,9 @@ import { environment } from 'src/environments/environment';
     AppRoutingModule,
     NavbarComponentModule,
     DateComponentModule,
-    AngularFireModule.initializeApp(environment.firestoreConfig),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firestoreConfig:{
-    apiKey: "AIzaSyATPj5dz1kWDtQMHIh2HN2AQD8C1goPX9A",
-    authDomain: "todo-app-cac7d.firebaseapp.com",
-    projectId: "todo-app-cac7d",
-    storageBucket: "todo-app-cac7d.appspot.com",
-    messagingSenderId: "342205891708",
-    appId: "1:342205891708:web:7cae7b1c35a68f804335f9",
-    measurementId: "G-6YLT3PFNN2"
-  }
+  firebase: {
+    projectId: 'lowgular-team',
+    appId: '1:229029381419:web:4a0524004e1a1dfd43f16e',
+    storageBucket: 'lowgular-team.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBudRiQYQ0sTGbrnxa8GGx3Gl1qEgIKTqU',
+    authDomain: 'lowgular-team.firebaseapp.com',
+    messagingSenderId: '229029381419',
+  },
+  production: false
 };
 
 /*
